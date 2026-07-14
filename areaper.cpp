ba86c9af888189226bb7547d638b5944a439ca8f
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-14 15:48:57 +0530

    Add files via upload

---
+++ areaper.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int l,b;
+cout<<"enter length and breadth";
+cin>>l,b;
+int area = l*b;
+int per = 2*l + 2*b;
+cout<< "area="<<area<<endl;
+cout<<"per="<<per;
+}
---
+++ circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int r;
+cout<<"enter radius";
+cin>>r;
+int area= 3.14*r*r;
+int cir= 2*3.14*r;
+cout<<"area="<<area<<endl;
+cout<<"cir="<<cir;
+}
+
