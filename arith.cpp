d926137e5f9b2f77aac719a261edb9d926025feb
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-14 15:43:18 +0530

    Add files via upload

---
+++ arith.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std; 
+int main() 
+{
+int a = 10;
+int b = 9;
+int sum = a + b;
+int mult = a*b;
+int sub = a-b;
+int div = a/b;
+int mod = a%b;
+cout << sum << "sum"<<endl;
+cout << mult <<"mult"<<endl;
+cout << sub <<"sub"<<endl;
+cout << div <<"div"<<endl;
+cout << mod << "mod"<<endl;
+return 0;
+}
+
