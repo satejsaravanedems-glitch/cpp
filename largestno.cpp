3e4b818980ef3c6e83ef980bcfbe2af046a13116
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-21 09:28:22 +0000

    change

---
+++ largestno.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    if(a>b){
+        cout<<"a is greater"<<endl;
+    }else{
+        cout<<"b is greater"<<endl;
+    }
+}
