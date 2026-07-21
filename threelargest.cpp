78a63478d4679b05c8e65fb3697ccbbe2cfc9e63
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-21 09:40:58 +0000

    change

---
+++ threelargest.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if(a>b && a>c){
+        cout<<"a is largest";
+    }
+    if(b>c && b>a){
+        cout<<"b is largest";
+
+    }else{
+        cout<<"c is largest";
+    }
+
+
+        
+    
+}
