22b9049218161b8b4af33071db12462d58469caa
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-21 10:07:23 +0000

    change

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    unsigned long long factorial=1;
+    cout<<"enter a positive integer:";
+    cin>>n;
+     for (int i = 1; i <= n; ++i) {
+        factorial *= i;
+        cout<< "Factorial of " << n << " = " << factorial << std::endl;
+    }
+}
