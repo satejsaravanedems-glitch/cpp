ec52a5d0694e0e969f4cbdbadf34a45939206448
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-21 09:47:03 +0000

    change

---
+++ posneg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if (a>0){
+        cout<<"a is positive";
+    }
+    if(a<0){
+        cout<<"a is negative";
+    }
+    if(a=0){
+        cout<<"a is zero";
+    }
+}
