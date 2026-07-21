e475eb079d063b02010f7e52d913512e91b98049
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-07-21 09:49:49 +0000

    change

---
+++ year.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%4==0){
+        cout<<"a is leap year";
+    }else{
+        cout<<" a is not a leap year";
+    }
+}
