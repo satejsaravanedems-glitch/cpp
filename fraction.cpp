c737b454b504e8e91a9a2d392369b966fc20d040
satejsaravanedems-glitch <satejsaravane.dems@gmail.com>
2026-08-18 09:28:33 +0000

    c

--- fraction.cpp
+++ fraction.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
-class fraction{
-    private:
-    int num, den;
-
-    public:
-
+void frac_num(int n1,int d1,int n2,int d2){
+cout<<"num = "<<(n1*d2)<<"+"<<(n2*d1)<<endl;}
+void frac_den(int n1,int d1,int n2,int d2){
+cout<<"den ="<<(d1*d2)<<endl;
 }
+int main(){
+int n1,n2,d1,d2;
+cout<<"enter first fraction";
+cin>>n1>>d1;
+cout<<"enter second fraction";
+cin>>n2>>d2;
+
+frac_num(n1,d1,n2,d2);
+frac_den(n1,d1,n2,d2);
+return 0;
+}
+    
